632dc9b9728d0ad4dd5df98f810e86509f9673df
mheblewski <heblewski.mateusz@gmail.com>
2025-11-20 07:46:23 +0100

    Adjust medical disclaimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import NSProfileTuner from "./NSProfileTuner";
+import MedicalDisclaimer from "./components/MedicalDisclaimer";
 
 export default function App() {
   return (
     <div
-      className="min-h-screen py-6 px-1 flex flex-col justify-between"
+      className="min-h-screen py-6 px-1 flex flex-col"
       style={{
         minHeight: "100vh",
         width: "100vw",
@@ -15,19 +16,14 @@ export default function App() {
         style={{
           flex: 1,
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
         <NSProfileTuner defaultDays={7} />
+        <MedicalDisclaimer />
       </div>
-      <footer className="w-full text-center text-xs text-gray-500 py-4 px-2 mt-4">
-        Ta aplikacja ma charakter informacyjny i nie powinna być używana jako
-        jedyne źródło decyzji terapeutycznych. Wszelkie zmiany zawsze konsultuj
-        ze swoim diabetologiem. Twórca nie ponosi odpowiedzialności za skutki
-        błędnego działania algorytmu ani za decyzje podjęte na podstawie wyników
-        aplikacji.
-      </footer>
     </div>
   );
 }
---
+++ src/components/MedicalDisclaimer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MedicalDisclaimer() {
+  return (
+    <div
+      className="w-full text-center text-xs text-gray-500 py-4 px-2 mt-4 mx-8"
+      role="note"
+    >
+      Ta aplikacja ma charakter informacyjny i nie powinna być używana jako
+      jedyne źródło decyzji terapeutycznych. Wszelkie zmiany zawsze konsultuj ze
+      swoim diabetologiem. Twórca nie ponosi odpowiedzialności za skutki
+      błędnego działania algorytmu ani za decyzje podjęte na podstawie wyników
+      aplikacji.
+    </div>
+  );
+}
